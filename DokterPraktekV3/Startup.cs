307d585b7c61071e1b542614dc4403bfa406ff92
lenovo <lenovo@DESKTOP-13SOBO7>
2018-11-10 15:09:11 +0700

    Add project files.

---
+++ DokterPraktekV3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DokterPraktekV3.Startup))]
+namespace DokterPraktekV3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
